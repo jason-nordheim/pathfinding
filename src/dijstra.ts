a3b769276193904a8ba338ecd8d489369fbec15e
jason-nordheim <jason.nordheim@gmail.com>
2020-12-03 23:42:19 -0700

    a ton of stuff

--- src/dijstra.ts
+++ src/dijstra.ts
@@ -1,103 +1,188 @@
 
-class Grid {
-    
-    /**
-     * The nodes contained within the grid 
-     */
-    private _nodes: Array<GridNode> = new Array<GridNode>(); 
+export class Grid {
+    constructor(rows:number, columns:number){
+        if(rows < 2) throw Error('rows must be greater than: 2')
+        if(columns < 2) throw Error('columns must be greater than: 2')
+        this._rows = Math.floor(rows)
+        this._columns = Math.floor(columns) 
+        this._start = null; 
+        this._end = null;
+        this._visited = new Array<GridNode>() 
+        this._unvisited = new Array<GridNode>() 
+        this.buildGrid(this.rows, this.columns)
+    }
+    protected buildGrid(rows:number, columns:number): void {
+        let x= 0, y = 0;
+        while(x < rows){
+            while(y < columns){
+                this._nodes[x][y] = new GridNode(x, y, this.rows, this.columns) 
+                this._unvisited.push(this._nodes[x][y])
+                y++; 
+            }
+            x++; 
+        }
+    }
+    protected reset() {
+        this._visited = new Array<GridNode>() 
+        this._unvisited = new Array<GridNode>() 
+        this.buildGrid(this.rows, this.columns)
+    }
+
+    protected _visited:Array<GridNode>
+    protected get visited(){
+        return this._visited
+    }
+    protected _unvisited: Array<GridNode> 
+    protected get unvisited(){
+        return this._unvisited
+    }
+    protected _rows:number
+    public get rows():number{
+        return this._rows 
+    }
+    protected _columns:number 
+    public get columns():number{
+        return this._columns
+    }
+
+    protected _nodes: Array<Array<GridNode>> = new  Array<Array<GridNode>>(); 
     public get nodes(){
         return this._nodes; 
     }
+    protected get arrayNodes(){
+        const flattenedNodeList = new Array<GridNode>() 
+        for (let x = 0; x < this.nodes.length; x++) {
+            for (let y = 0; y < this.nodes[x].length; y++) {
+                const node = this.nodes[x][y];
+            }
+        }
+        return flattenedNodeList
+    } 
 
-    /**
-     * The starting node 
-     */
-    private _start: GridNode | null | undefined = null; 
+    protected _start: GridNode | null 
     public get start(){
-        if (!this._start) throw Error('No start node set')
+        if (!this._start) throw Error('start node is null')
         return this._start; 
     }
-    public set start(sNode: GridNode){
-        this._start = sNode; 
+    public set start(start: GridNode){
+        this._nodes[start.position.x]
+        this._start = start; 
     }
 
-
-    /**
-     * The ending node 
-     */
-    private _end: GridNode | null | undefined = null;
+    protected _end: GridNode | null
     public get end(){
-        if (!this._end) throw Error('No start node set')
+        if (!this._end) throw Error('end node is null')
         return this._end; 
     }
     public set end(sNode: GridNode){
         this._end = sNode; 
     }
 
-    private getNodeWithShortestPath(){
-        if(this._nodes.length === 0) throw new Error('No nodes on grid')
+    public getNeighbors(node:GridNode) : Array<GridNode>  {
+        const x = node.x, y = node.y
+        const neighbors = new Array<GridNode>(); 
+        if (x + 1 < this.rows  && this.nodes[x][y]) {
 
-        let node = this._nodes[0]
-        for(let i = 0; i < this._nodes.length; i++){
-            
         }
+        return neighbors; 
     }
 
-    // private findNeighbors(node:GridNode){
-    //     const neighbors = new Array<GridNode>(); 
-    //     if(node.position.x - 1 )
-    // }
+    protected distanceBetween(nodeA:GridNode, nodeB:GridNode):number {
+        if(nodeA.x === nodeB.x && nodeB.y === nodeB.y) return 0 
+        const x = Math.abs(nodeA.x - nodeB.x)
+        const y = Math.abs(nodeA.y -nodeB.y) 
+        return x + y 
+    }
 
+    protected sortUnvisited(nodes:Array<Array<GridNode>>, start:GridNode, end:GridNode){
+
+    }
+
+    protected dijstra() {
+        while(!!this._unvisited.length){
+            sortNodesByDistance(this.unvisitedNodes, grid.start, grid.end)
+        }
+    }
+    
 }
 
 enum GridNodeType {
     default, 
     barrier, 
     open, 
+    weighted, 
     closed, 
     path 
 }
 
-class GridNode {
-
-    /**
-     * Position of a node on a grid 
-     */
-    private _position:Position | null = null 
-    public get position(){
-        if(!this._position) throw new Error('position is null')
-        return this._position
+class GridPosition {
+    constructor(x:number, y:number){
+        this._x = x 
+        this._y = y 
     }
-    public set position(pos:Position){
-        this._position = pos
+    protected _x:number; 
+    public get x() : number {
+        return this._x
     }
-
-    /**
-     * The manhattan distance from current node 
-     * back to the starting node. defaults to infinity 
-     */
     
-    private _distance : number = 0; 
+    protected _y:number; 
+    public get y() : number {
+        return this._y; 
+    }
+    
+}
+
+class GridNode extends GridPosition {
+    constructor(row:number, col:number, totalRows:number, totalColumns: number, weight?: number){
+        if(row >= totalRows) throw new Error('row number cannot exceed total rows')
+        if(col >= totalColumns) throw new Error('columns number cannot exceed total columns')
+        super(row, col)
+        this._startDistance = Infinity; 
+        this._endDistance = Infinity; 
+        this._type = GridNodeType.default; 
+        this._visits = 0; 
+        this._weight = weight ? weight : 1; 
+    }
+
     public get distance() : number {
-        return this._distance;
-    }
-    public set distance(v : number) {
-        this._distance = v;
+        return this.startDistance + this.endDistance; 
     }
 
-    // /**
-    //  * The node from from which the path came from
-    //  */
-    // private _pathFrom: GridNode | null = null; 
-    // public get pathFrom(){
-    //     if(!this._pathFrom) throw new Error('pathFrom is null')
-    //     return this._pathFrom; 
-    // }
-    // public set pathFrom(node:GridNode){
-    //     this._pathFrom = node; 
-    // }
+    protected _startDistance: number 
+    public get startDistance(): number {
+        return this._startDistance; 
+    }
+    public set startDistance(distance:number){
+        this.startDistance = distance; 
+    }
+    
+    protected _endDistance : number 
+    public get endDistance() : number {
+        return this._endDistance;
+    }
+    public set endDistance(distance : number) {
+        this._endDistance = distance;
+    }
 
-    private _type: GridNodeType | null = null; 
+    protected _weight: number; 
+    public get weight() : number {
+        return this._weight; 
+    }
+    
+    public get weightedDistance():number {
+        return this._weight * this.distance; 
+    }
+
+    protected _visits : number;
+    public get visits() : number {
+        return this._visits;
+    }
+    public visit(){
+        return this._visits++ 
+    }
+
+    
+    protected _type: GridNodeType; 
     public get type(){
         if(!this._type) throw new Error('pathFrom is null')
         return this._type; 
@@ -105,38 +190,48 @@ class GridNode {
     public set type(type:GridNodeType){
         this._type = type;  
     }
-
 }
 
-class Position {
-    x:number; 
-    y:number; 
-    constructor(x:number, y:number){
-        this.x = x 
-        this.y = y 
+
+
+
+
+class DomGrid extends Grid {
+    constructor(element:HTMLElement, rows:number, columns:number){
+        super(rows, columns)
+        this._element = element; 
+    }
+    private _element:HTMLElement 
+    public get element() : HTMLElement {
+        return this._element
+    }
+
+    protected buildGrid(rows:number, columns:number): void {
+        let x= 0, y = 0;
+        while(x < rows){
+            while(y < columns){
+                this._nodes[x][y] = new DomGridNode(this.element, x, y) 
+                y++; 
+            }
+            x++; 
+        }
     }
 }
 
-
-export default function dijstra(grid: Grid) {
-    const unvisitedNodes = [...grid.nodes] 
-    const visitedNodes = new Array<GridNode>(); 
-    while(!!unvisitedNodes.length){
-        sortNodesByDistance(unvisitedNodes)
+class DomGridNode extends GridNode {
+    constructor(parent:HTMLElement, row:number, col:number, weight?: number){
+        super(row, col, weight) 
+        const self = document.createElement('div')
+        self.classList.add('node')
+        parent.appendChild(self) 
+        this._element = self 
     }
-}
-
-function sortNodesByDistance(nodes:Array<GridNode>){
-    throw new Error('Not implemented')
-}
-
-/**
- * Gets the distance between two GridNode objects 
- * @param nodeA 
- * @param nodeB 
- */
-function getDistance(nodeA:GridNode, nodeB:GridNode):number{
-    const x = Math.abs(nodeA.position.x - nodeB.position.x)
-    const y = Math.abs(nodeA.position.y -nodeB.position.y) 
-    return x + y 
+    
+    protected _element: HTMLElement 
+    protected get element(): HTMLElement {
+        return this._element; 
+    }
+    protected set element(ele:HTMLElement): HTMLElement {
+        this._element = ele
+    }
 }
