9462edb96762bda1c38f796a305803d42495c846
jason-nordheim <jason.nordheim@gmail.com>
2020-12-30 13:01:09 -0700

    removed a bunch of tests that have nothing to do with the grid

--- src/Grid.test.js
+++ src/Grid.test.js
@@ -1,14 +1,11 @@
 const { Grid } = require("./Grid")
-const { GridNode } = require("./GridNode")
-
-
 
 describe(`Grid constructs a grid `, () => {
     describe('given valid rows and columns', () => {
         const rows = 10
         const cols = 10
         const g = new Grid(rows, cols)
-        test('grid object is truthy', () => {
+        test('Grid object is truthy', () => {
             expect(g).toBeDefined()
             expect(g).toBeTruthy() // 
         })
@@ -20,88 +17,10 @@ describe(`Grid constructs a grid `, () => {
             expect(g.rows).toBe(rows)
         })
         test('new Grid initialized without start position', () => {
-            const s = g.start
-            expect(s).toBeUndefined()
+            expect(g.start).toBeFalsy()
         })
         test('initialized without end position', () => {
-            const e = g.end
-            expect(e).toBeUndefined()
-        })
-        test('initialized solely with GridNodes of type \'open\' GridNodes', () => {
-            for (let r = 1; r < rows; r++) {
-                for (let c = 1; c < cols; c++) {
-                    expect(g.getNode(r, c)).toBeTruthy()
-                    expect(g.getNode(r, c)).toBeInstanceOf(GridNode)
-                    expect(g.getNode(r, c).type).toBe('open')
-                }
-            }
-        })
-
-
-        describe('start position can be set', () => {
-            const start = { row: 8, col: 7 }
-            test(`can set start position to row:${start.row} and col:${start.col}`, () => {
-                const setStart = () => g.start = start
-                expect('setting start does not throw error', () => {
-                    setStart()
-                })
-            })
-            test('start property is defined', () => {
-                expect(g.start).toBeDefined()
-            })
-            test('start property has defined row attribute', () => {
-                expect(g.start.row).toBeDefined()
-                expect(g.start.row).toEqual(start.row)
-            })
-            test('start property has defined column attribute', () => {
-                expect(g.start.col).toBeDefined()
-                expect(g.start.col).toEqual(start.col)
-            })
-            describe('setting start node does not change other nodes', () => {
-                for (let r = 1; r < rows; r++) {
-                    for (let c = 1; c < cols; c++) {
-                        test(`node at ${r},${c} is defined`, () => {
-                            expect(g.getNode(r, c)).toBeTruthy()
-                            expect(g.getNode(r, c)).toBeDefined()
-                        })
-                        const expectedType = start.row === r && start.col === c
-                            ? 'start'
-                            : 'open'
-                        test(`node at ${r},${c} has expected type: '${expectedType}'`, () => {
-                            expect(g.getNode(r, c).type).toBe(expectedType)
-                        })
-                    }
-                }
-            })
+            expect(g.end).toBeFalsy()
         })
     })
-    // const end = { row: 2, col: 3 }
-    // describe(`can set end position to row:${end.row} and col:${end.col}`, () => {
-    //     g.end = end
-    //     test('end property is defined', () => {
-    //         expect(g.end).toBeDefined()
-    //     })
-    //     test('end property has defined row attribute', () => {
-    //         expect(g.end.row).toBeTruthy()
-    //         expect(g.end.row).toEqual(end.row)
-    //     })
-    //     test('end property has defined column attribute', () => {
-    //         expect(g.end.row).toBeTruthy()
-    //         expect(g.end.row).toEqual(end.row)
-    //     })
-    //     test('setting start node does not change other nodes', () => {
-    //         for (let r = 1; r < rows; r++) {
-    //             for (let c = 1; c < cols; c++) {
-    //                 expect(g.getNode(r, c)).toBeTruthy()
-    //                 if (start.row == r && start.col == c) {
-    //                     expect(g.getNode(r, c).type).toEqual('start')
-    //                 } else if (end.row == r && end.col == c) {
-    //                     expect(g.getNode(r, c).type).toEqual('end')
-    //                 } else {
-    //                     expect(g.getNode(r, c).type).toEqual('open')
-    //                 }
-    //             }
-    //         }
-    //     })
-    // })
 })
